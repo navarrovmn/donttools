5adfb93ae8775affbe5c73d67849dd865179c016
Victor Navarro <victor.matias.navarro@gmail.com>
2018-07-05 09:41:36 -0300

    Changing models

--- kanban/models.py
+++ kanban/models.py
@@ -3,12 +3,22 @@ from django.db import models
 
 class Board(models.Model):
     title = models.CharField(max_length=140)
-    ...
 
 
 class Issue(models.Model):
+    KIND_TODO = 0
+    KIND_DOING = 1
+    KIND_DONE = 2
+    KIND_CHOICES = [
+        (KIND_TODO, ('To Do')),
+        (KIND_DOING, ('Doing')),
+        (KIND_DONE, ('Done')),
+    ]
+    KIND_MAP = dict(KIND_CHOICES)
+
     title = models.CharField(max_length=140)
+    kind = models.PositiveIntegerField(choices=KIND_CHOICES)
+    kind_name = property(lambda self: self.kind_display())
     description = models.TextField()
     is_active = models.BooleanField()
-    status = ... # to-do, doing or done?
-    ...
+    status = models.CharField(max_length=140)
